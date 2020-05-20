0fde4a5e3256f5d34730d145c1b23d7c3462abcb
Sam Fowler <sam.fowler91@hotmail.co.uk>
2020-05-20 03:26:44 +0100

    Remove cout spam

--- src/Game.cpp
+++ src/Game.cpp
@@ -15,16 +15,19 @@ bool Game::update(Input& input)
         m_is_running = false;
     }
 
+    /*
     if (input.isKeyHeld(SDLK_SPACE))
     {
         std::cout << "Space bar helds" << std::endl;
     }
-
+    */
+    
     if (input.wasButtonPressed(SDL_BUTTON_LEFT))
     {
         glm::ivec2 mouse_pos = input.getMousePos();
         std::cout << "Left mouse button pressed at position: " << mouse_pos.x << "," << mouse_pos.y << std::endl;
     }
+    
 
     return m_is_running;
 }
