f9dc25c09adb4f704043fe2ecd3087e97bfba90c
Sam Fowler <sam.fowler91@hotmail.co.uk>
2020-06-01 02:35:20 +0100

    Clean and comment the greedy mesher

--- src/renderers/greedy_meshing.cpp
+++ src/renderers/greedy_meshing.cpp
@@ -11,26 +11,24 @@ struct ValRLpair
 {   
     ValRLpair() : val(0), rl(0), wid(0) {};
     ValRLpair(unsigned value, unsigned run_length) : val(value), rl(run_length), wid(0) {};
-    ValRLpair(unsigned value, unsigned run_length, unsigned width, bool is_a_rectangle = false) : val(value), rl(run_length), wid(width),
-    is_rect(is_a_rectangle) {};
+    ValRLpair(unsigned value, unsigned run_length, unsigned width) : val(value), rl(run_length), wid(width) {};
     unsigned val = 0;
     unsigned rl = 0;
     unsigned wid = 0;
-    bool is_rect = false;
 };
 
 int main()
 {
-    unsigned side_size = 5;
+    unsigned side_size = 8;
 
-    /*std::vector<int> data = { 1, 1, 1, 2, 1, 1, 1, 1,
+    std::vector<int> data = { 1, 1, 1, 2, 1, 1, 1, 1,
                               1, 1, 1, 1, 1, 1, 2, 2,
                               1, 1, 3, 3, 3, 2, 1, 2,
                               3, 3, 2, 1, 1, 0, 0, 0,
                               1, 1, 1, 2, 3, 3, 3, 0,
                               1, 1, 2, 2, 2, 3, 3, 3, 
                               0, 0, 0, 2, 2, 4, 4, 4,
-                              0, 0, 2, 1, 1, 4, 4, 1 };*/
+                              0, 0, 2, 1, 1, 4, 4, 1 };
 /* 
    std::vector<int> data = {  1, 1, 1, 1, 1, 1, 1, 1,
                               1, 1, 1, 1, 1, 1, 1, 1,
@@ -41,12 +39,12 @@ int main()
                               1, 1, 1, 1, 1, 2, 1, 1,
                               1, 1, 1, 1, 1, 1, 1, 1 }; */
 
-    std::vector<int> data = {0, 0, 1, 0, 0,
+  /*   std::vector<int> data = {0, 0, 1, 0, 0,
                              0, 0, 2, 2, 0,
                              0, 1, 1, 1, 0,
                              0, 1, 2, 1, 0,
                              0, 2, 1, 1, 0
-                                    };
+                                    }; */
 
     LOG(data.size());
     std::cout << std::endl;
@@ -84,20 +82,18 @@ int main()
     unsigned x = 0;
 
     unsigned voxel_val = 0;
-    unsigned index = 0;
 
 
     for (x = 1; x < side_size; x++)
     {
-        index = xzToIndex(x,z,side_size);
 
-        voxel_val = data[index];
+        voxel_val = data[xzToIndex(x,z,side_size)];
 
         run_length++;
 
         if ( (voxel_val != prev_voxel_val))
         {   
-            holder[xzToIndex( (x-run_length), z, side_size )] = {prev_voxel_val, run_length, 1, true};
+            holder[xzToIndex( (x-run_length), z, side_size )] = {prev_voxel_val, run_length, 1};
             run_length = 0;
         }
 
@@ -106,66 +102,58 @@ int main()
 
 
     }
-    holder[xzToIndex( ((x-1)-run_length), z, side_size )] = {prev_voxel_val, (run_length+1), 1, true};
+    holder[xzToIndex( ((x-1)-run_length), z, side_size )] = {prev_voxel_val, (run_length+1), 1};
 
     std::cout << std::endl;
-    unsigned width = 0;
 
-    ValRLpair last_row_rectangle;
     ValRLpair previous_last_row_rectangle;
-    unsigned negative_rl_counter =0;
 
-    unsigned rl_to_reach;
-    unsigned rl_counter;
-
+    /* ------------START Z LOOP----------- */
     for (unsigned z = 1; z < side_size; z++)
     {   
 
-        previous_last_row_rectangle = {0,0,0,false};
+        previous_last_row_rectangle = {0,0,0};
         run_length = 0;
         x = 0;
-        rl_to_reach = 0;
-        rl_counter  = 0;
 
-        index = xzToIndex(x,z,side_size);
-        voxel_val = data[index];
-        //run_length++;
+        /* ------------START X = 0 ----------- */
+        // run x=0 separately before loop so we don't have extra if checks for it in each loop
+        voxel_val = data[xzToIndex(x,z,side_size)];
 
-
-        last_row_rectangle = holder[xzToIndex(x, z-1, side_size)];
-
-        if ( voxel_val == last_row_rectangle.val )
+        if ( voxel_val == holder[xzToIndex(x, z-1, side_size)].val )
         {
-            previous_last_row_rectangle = last_row_rectangle;
+            previous_last_row_rectangle = holder[xzToIndex(x, z-1, side_size)];
             previous_last_row_rectangle.wid++; 
-            rl_counter = 0;      
-        }
-        else 
-        {
-            //previous_last_row_rectangle = {voxel_val, 1, 1, true};
         }
 
         prev_voxel_val = data[xzToIndex(0, z, side_size)];
+        /* ------------END X = 0----------- */
 
+
+
+        /* ------------START X = 1 onwards LOOP----------- */
         for (unsigned x = 1; x < side_size; x++)
         {
-            index = xzToIndex(x,z,side_size);
-            voxel_val = data[index];
+            voxel_val = data[xzToIndex(x,z,side_size)];
             run_length++;
-            rl_counter++;
 
 
             if (voxel_val == prev_voxel_val)
             {
-
-                if ( (previous_last_row_rectangle.wid > 0) )//&& (rl_counter == previous_last_row_rectangle.rl))
-                {
-                    if (rl_counter == previous_last_row_rectangle.rl)
+                // Check if we are currently trying to expand a rectangle from the previous column
+                if ( (previous_last_row_rectangle.wid > 0) ) 
+                {   
+                    // if the voxels in this row have reached the same run length as the rectangle we are trying to expand,
+                    // we can add these voxels to that rectangle
+                    if (run_length == previous_last_row_rectangle.rl) 
                     {
-                        //WE HAVE A SET THAT MATCHES A PREVIOUS ROW RECTANGLE - place it in
-                        holder[xzToIndex( (x-rl_counter), z, side_size )] = previous_last_row_rectangle;
+                        //We have a group of voxels that match the previous column rectangle - place them in
+                        //TODO can we replace the original rectangle placeholder rather than creating a new one?
+                        holder[xzToIndex( (x-run_length), z, side_size )] = previous_last_row_rectangle;
 
-                        // as runlength of previous row has been reached, a new rectanlge must be there
+                        // as the runlength of the rectangle from previous column has been reached, 
+                        // a new, different, rectangle must be at this row value in the previous column -
+                        // We fetch it if it is of the same voxel type, else set rectangle to empty so we don't look to match to it
                         if (voxel_val == data[xzToIndex(x, z-1, side_size)])
                         {
                             previous_last_row_rectangle = holder[xzToIndex(x, z-1, side_size)];  
@@ -175,45 +163,40 @@ int main()
                         {
                             previous_last_row_rectangle = {0, 0, 0};
                         }
-                        rl_counter = 0;
                         run_length = 0;
                     }
                     else
                     {
-                        //std::cout << "here" << std::endl;
                         //carry on...?
                     }
                     
                     
                 }
+                // if we are not trying to match to rectangle from previous column,
+                // get the rectangle from the previous column and check if the voxels match with this voxel
                 else if (voxel_val == holder[xzToIndex(x, z-1, side_size)].val)
                 {
                     previous_last_row_rectangle = holder[xzToIndex(x, z-1, side_size)];
                     previous_last_row_rectangle.wid++;
 
-                    holder[xzToIndex( (x-run_length), z, side_size )] = {prev_voxel_val, run_length, 1, true};
-                    rl_counter = 0;
+                    holder[xzToIndex( (x-run_length), z, side_size )] = {prev_voxel_val, run_length, 1};
                     run_length = 0;
                 } 
                 else
                 {
-                    //holder[xzToIndex( (x-run_length), z, side_size )] = {prev_voxel_val, run_length, 1, true};
-                    //rl_counter = 0;
-                    //run_length = 0;
+ 
                 }
                 
             }
-            else //(voxel_val != prev_voxel_val)
+            else
             {
-                //holder[xzToIndex( (x-run_length), z, side_size )] = {prev_voxel_val, run_length, 1, true};
-                //run_length = 0;
 
-                if ( (previous_last_row_rectangle.wid > 0) )//&& (rl_counter == previous_last_row_rectangle.rl))
+                if ( (previous_last_row_rectangle.wid > 0) )
                 {
-                    if (rl_counter == previous_last_row_rectangle.rl)
+                    if (run_length == previous_last_row_rectangle.rl)
                     {
                         //WE HAVE A SET THAT MATCHES A PREVIOUS ROW RECTANGLE - place it in
-                        holder[xzToIndex( (x-rl_counter), z, side_size )] = previous_last_row_rectangle;
+                        holder[xzToIndex( (x-run_length), z, side_size )] = previous_last_row_rectangle;
 
                         // as runlength of previous row has been reached, a new rectanlge must be there
                         if (voxel_val == data[xzToIndex(x, z-1, side_size)])
@@ -225,16 +208,13 @@ int main()
                         {
                             previous_last_row_rectangle = {0, 0, 0};
                         }
-                        rl_counter = 0;
                         run_length = 0;
                     }
                     else
                     {
                         previous_last_row_rectangle = {0, 0, 0};
-                        holder[xzToIndex( (x-run_length), z, side_size )] = {prev_voxel_val, run_length, 1, true};
-                        rl_counter = 0;
+                        holder[xzToIndex( (x-run_length), z, side_size )] = {prev_voxel_val, run_length, 1};
                         run_length = 0;
-                        //carry on...?
                     }
                     
                     
@@ -244,83 +224,58 @@ int main()
                     previous_last_row_rectangle = holder[xzToIndex(x, z-1, side_size)];
                     previous_last_row_rectangle.wid++;
 
-                    holder[xzToIndex( (x-run_length), z, side_size )] = {prev_voxel_val, run_length, 1, true};
-                    rl_counter = 0;
+                    holder[xzToIndex( (x-run_length), z, side_size )] = {prev_voxel_val, run_length, 1};
                     run_length = 0;
                 }
                 else
                 {
-                    holder[xzToIndex( (x-run_length), z, side_size )] = {prev_voxel_val, run_length, 1, true};
-                    rl_counter = 0;
+                    holder[xzToIndex( (x-run_length), z, side_size )] = {prev_voxel_val, run_length, 1};
                     run_length = 0;
                 }
 
             }
             
-
-
-
-            
-            
-            
-        
-
             prev_voxel_val = voxel_val;
 
-    
-
-
-           
-
-
         }
-
-            run_length++;
-            rl_counter++;
+            /* ------------START X = SIDE_SIZE ----------- */
+            // need special case for this as it needs to fill in the rectangle array before starting next z loop
             x = side_size;
-                if ( (previous_last_row_rectangle.wid > 0) )//&& (rl_counter == previous_last_row_rectangle.rl))
-                {
-                    if (rl_counter == previous_last_row_rectangle.rl)
-                    {
-                        //WE HAVE A SET THAT MATCHES A PREVIOUS ROW RECTANGLE - place it in
-                        holder[xzToIndex( (x-rl_counter), z, side_size )] = previous_last_row_rectangle;
+            run_length++;
 
-                        // as runlength of previous row has been reached, a new rectanlge must be there
-                        if (voxel_val == data[xzToIndex(x, z-1, side_size)])
-                        {
-                            previous_last_row_rectangle = holder[xzToIndex(x, z-1, side_size)];  
-                            previous_last_row_rectangle.wid++; 
-                        }
-                        else 
-                        {
-                            previous_last_row_rectangle = {0, 0, 0};
-                        }
-                        rl_counter = 0;
-                        run_length = 0;
-                    }
-                    else
-                    {
-                        //carry on...?
-                    }
-                    
-                    
-                }
-                else //if// (voxel_val == holder[xzToIndex(x, z-1, side_size)].val)
+            if ( (previous_last_row_rectangle.wid > 0) )
+            {
+                if (run_length == previous_last_row_rectangle.rl)
                 {
-                    //previous_last_row_rectangle = holder[xzToIndex(x, z-1, side_size)];
-                    //previous_last_row_rectangle.wid++;
+                    //WE HAVE A SET THAT MATCHES A PREVIOUS ROW RECTANGLE - place it in
+                    holder[xzToIndex( (x-run_length), z, side_size )] = previous_last_row_rectangle;
 
-                    holder[xzToIndex( (x-run_length), z, side_size )] = {prev_voxel_val, run_length, 1, true};
-                    rl_counter = 0;
+                    // as runlength of previous row has been reached, a new rectanlge must be there
+                    if (voxel_val == data[xzToIndex(x, z-1, side_size)])
+                    {
+                        previous_last_row_rectangle = holder[xzToIndex(x, z-1, side_size)];  
+                        previous_last_row_rectangle.wid++; 
+                    }
+                    else 
+                    {
+                        previous_last_row_rectangle = {0, 0, 0};
+                    }
                     run_length = 0;
                 }
+                else {/*carry on...?*/}
+            }
+            else 
+            {
+                holder[xzToIndex( (x-run_length), z, side_size )] = {prev_voxel_val, run_length, 1};
+                run_length = 0;
+            }
             
 
 
     }
 
 
-
+    /* ------------PRINTING----------- */
     for (unsigned z = 0; z < side_size; z++)
     {
         for (unsigned x = 0; x < side_size; x++)
@@ -333,5 +288,6 @@ int main()
         }
         std::cout << std::endl;
     }
+    /* ------------END PRINTING----------- */
 
 }
